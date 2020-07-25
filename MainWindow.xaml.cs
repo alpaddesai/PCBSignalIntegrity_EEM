7cc1d166b53835984a54fa6133f5223913de4ad8
alpaddesai <alpa.gupta@gmail.com>
2020-07-25 08:58:04 -0400

    second commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace PCBSignalIntegrity_EEM
         {
             InitializeComponent();
         }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
